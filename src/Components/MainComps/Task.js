a33533d1fcdf5f2fd479b05164c9812f54c2c13f
kkatoch89 <karankatoch.hba@gmail.com>
2020-09-27 18:07:31 -0400

    Back baby! Reset project

--- src/Components/MainComps/Task.js
+++ src/Components/MainComps/Task.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 const Task = (props) => {
-	return <li>{props.task}</li>;
+	return (
+		<li>
+			{props.task}
+			<FontAwesomeIcon icon={faTrashAlt} />
+		</li>
+	);
 };
 
 export default Task;
